2a6c12ec57d2a2bc10c2c8ed6d54e786f68fbfc4
Atlas <juan.gomez-sanchez@isen.yncrea.fr>
2023-05-09 14:40:31 +0200

    euh j'espère c'est compréhensible

--- src/Save/save.c
+++ src/Save/save.c
@@ -4,19 +4,19 @@
 //gcc -o save save.c ../Jeu/jeu.c
 
 
-int retour_plateau(CASE plateau[8][8])
+int** retour_plateau(CASE** tab[8][8])
 {
-     CASE tab[8][8];
+    CASE plateau[8][8];
 
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-          tab[i][j].pion = plateau[i][j].pion;
+            tab[i][j] = plateau[i][j].pion;
         }
         
     }
-   return tab[8][8].pion;
+   return tab;
 }
 
 int isFileEmpty(char* filename) {
@@ -24,7 +24,6 @@ int isFileEmpty(char* filename) {
     if (file == NULL) {
         return -1; // Erreur d'ouverture du fichier
     }
-    
     fseek(file, 0, SEEK_END); // Se positionner à la fin du fichier
     if (ftell(file) == 0) { // Vérifier la position courante
         fclose(file);
@@ -35,7 +34,7 @@ int isFileEmpty(char* filename) {
     return 0; // Le fichier n'est pas vide
 }
 
-coup save_coup( coup *chaine, CASE plateau[8][8])
+coup save_coup(coup *chaine, CASE plateau[8][8])
 {
     //variable joueur à faire
     coup *c;
@@ -43,79 +42,60 @@ coup save_coup( coup *chaine, CASE plateau[8][8])
     c->plateau = retour_plateau(plateau);
     c->prec = chaine;
     chaine = c;
-    sauvegarde_txt(c);
     return  *chaine;
 }
 
 
 void sauvegarde_txt(coup *c){
     // ce pg va automatiquement sauvegarder le coup dans un fichier texte
-    
+
     FILE *fichier = fopen("../sauv/sauv.txt", "w");
-    fprintf(fichier, "%d:", c->tour);
-   // fprintf(fichier, "%d:", c->plateau);
+    while (c->prec != NULL)
+    {
+        fprintf(fichier, "%d:", c->tour);
+        // fprintf(fichier, "%d:", c->plateau);
         for(int i; i < 8; i++){
             for(int j; j < 8; j++){
-                fprintf(fichier, "%d", c->plateau);
+                fprintf(fichier, " %d ", c->plateau);
             }
         }
+        fprintf(fichier, "\n");
+    }
+    
     fclose(fichier);
 }
 
 
-void retour_coup(coup c,CASE plateau[8][8]){
-    
+
+CASE** retour_coup(coup c){
+    CASE plate[8][8];
     // on va rejouer le coup précédent
     //coup a;
     coup *a;
     
     a = c.prec;
-    int tab[8][8];
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            tab[i][j] = plateau[i][j].pion;
-        }
-    }
 
-
+    charger_partie(c);
     
-    //je vais supprimer la dernière ligne.
-    FILE *fichier = fopen("../sauv/sauv.txt", "w");
-    long int pos = -1;
-    int ch = 0;
+    return plate;
+}
 
-    fseek(fichier, 0, SEEK_END);
-    pos = ftell(fichier);
-
-    while (pos) {
-        fseek(fichier, --pos, SEEK_SET);
-        ch = fgetc(fichier);
-        if (ch == '\n') {
-            break;
-        }
-    }
-
-    fseek(fichier, pos, SEEK_SET);
-
-    ftruncate(fileno(fichier), pos);
-    fclose(fichier);
-    }
-
-void charger_partie(int tab[8][8], CASE plate[8][8])
-{
+CASE** charger_partie(coup c)
+{   
     // nous allons imiter le initCell du jeu de base, sauf que au lieu de le mettre tout en vide nous allons suivre le tableau sauvegardé dans le tableau
-    
+    CASE plate[8][8];
+
     for(int i = 0; i < 8; i++)
     {
         for(int j = 0;j < 8; j++)
         {
-            plate[i][j].pion = tab[i][j];
+            plate[i][j].pion = c.plateau[i][j];
             plate[i][j].posX = i;
             plate[i][j].posY = j; 
         }
     }
+    return plate;
+
 }
 int main()
 {
--- src/Save/save.h
+++ src/Save/save.h
@@ -17,18 +17,35 @@ typedef struct coup
     struct coup* prec;
 }coup;
 
-CASE** retour_plateau(int tab[8][8]);
+int** retour_plateau(CASE** tab[8][8]);
 /**
- * @brief il va retourner le 
+ * @brief il va retourner le  l'état de chaque case au plateau. supposé être un tableau de 64 chiffres
  * @param 
 */
 
+int isFileEmpty(char* filename);
+/**
+ * @brief prg qui retour un entier pour saboir si le fichier est vide ou non
+ * si 1 fichier vide, si 0 pas vide, si -1 erreur
+*/
+
 coup save_coup(coup* chaine, CASE plate[8][8]);
+/**
+ * @brief à la fin de chaque coup, nous allons donc ajouter le coup avec le round et le tableau avec l'état de chaque case.
+*/
 
 void sauvegarde_txt(coup* c);
+/**
+ * @brief nous allons sauvegarder tout les coups de la fin au début dans le fichier. 
+*/
 
-void retour_coup(coup c,CASE plateau[8][8]);
+CASE** retour_coup(coup c);
+/**
+ * @brief pour le coup en arrière, qui utilise charger partie
+*/
 
-void charger_partie(int tab[8][8], CASE plate[8][8]);
-
+CASE** charger_partie(coup c);
+/**
+ * @brief retourne un CASE avec tout les état du plate comme ça on a qu'à faire un init cell avec le CASE déjà prêt
+*/
 #endif;
